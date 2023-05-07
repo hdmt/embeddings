80860a079283cc9a1605ea0177dfd2215ab88811
hdmt <38484570+hdmt@users.noreply.github.com>
2023-05-07 16:39:51 +0900

    add ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-require('dotenv').config();
-const { Configuration, OpenAIApi } = require('openai');
+import * as dotenv from 'dotenv';
+import { Configuration, OpenAIApi } from 'openai';
+
+dotenv.config();
+
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
 const openai = new OpenAIApi(configuration);
 
 const main = async () => {
